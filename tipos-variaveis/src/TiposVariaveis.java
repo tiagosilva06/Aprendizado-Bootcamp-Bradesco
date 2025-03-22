50e2501e25704c321b4544071a7d1fc9d26a36d8
Tiago Santos <tiagosilva9278@gmail.com>
2025-03-22 10:57:23 -0300

    Mostrando os diferentes tipos de variaveis.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Tipos e variaveis");
+        System.out.println("{byte} idade = 123");
+        System.out.println("{short} ano = 2025");
+        System.out.println("{int} cep = 0152810");
+        System.out.println("{long} cpf = 58306435898L - L para especificar que é um long");
+        System.out.println("{float} pi = 3.14F ");
+        System.out.println("{double} salario = 1518.50");
     }
 }
