165e36f481f86d17bfed352dd4f93eb8cfdd047e
Tiago Santos <tiagosilva9278@gmail.com>
2025-03-23 08:36:02 -0300

    Introduzindo o conceito de "final"

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -7,5 +7,24 @@ public class TiposVariaveis {
         System.out.println("{long} cpf = 58306435898L - L para especificar que é um long");
         System.out.println("{float} pi = 3.14F ");
         System.out.println("{double} salario = 1518.50");
+
+        //Java é fortemente tipado
+       
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // é possivel converter um short para um int MAS 
+        // Não um int para um short - (apenas com o recurso cast)
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        // "final" - para n permitir mudanças na variavel
+
+
+
     }
 }
