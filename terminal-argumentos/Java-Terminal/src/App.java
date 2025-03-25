15b1094876a83f8741ebd5e9b1e9596fa42b9826
Tiago Santos <tiagosilva9278@gmail.com>
2025-03-25 20:29:14 -0300

    Finalizando o módulo de conceitos básicos do Bootcamp - Bradesco

---
+++ terminal-argumentos/Java-Terminal/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá pessoal, fui executado pelo terminal");
+        //Neste exercicio foi ensinado como executar minha classe fora da IDE (pelo prompt de comando/ PowerShell)
+    }
+}
